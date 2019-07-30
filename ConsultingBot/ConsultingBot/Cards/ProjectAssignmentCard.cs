8fc03f7f9dc9ffc52cd7d7856a9a6efa8b8dc678
Bob German <bgerman1@hotmail.com>
2019-07-29 21:29:50 -0400

    Now sends confirmation card at end of addToProject

--- ConsultingBot/ConsultingBot/Cards/ProjectAssignmentCard.cs
+++ ConsultingBot/ConsultingBot/Cards/ProjectAssignmentCard.cs
@@ -51,23 +51,23 @@ namespace ConsultingBot.Cards
                     new AdaptiveChoice()
                     {
                         Title = "Architect",
-                        Value = "1"
+                        Value = "Architect"
                     },
                     new AdaptiveChoice()
                     {
                         Title = "Developer",
-                        Value = "2"
+                        Value = "Developer"
                     },
                     new AdaptiveChoice()
                     {
                         Title = "Project Manager",
-                        Value = "3"
+                        Value = "Project Manager"
                     },
                 }
             });
 
-            var monthName = DateTime.Now.ToString("MMMM");
-            card.Body.Add(new AdaptiveTextBlock($"Forecast for {monthName}"));
+            var month1Name = DateTime.Now.ToString("MMMM");
+            card.Body.Add(new AdaptiveTextBlock($"Forecast for {month1Name}"));
             card.Body.Add(new AdaptiveCards.AdaptiveTextInput()
             {
                 Id = "forecast1",
@@ -75,16 +75,16 @@ namespace ConsultingBot.Cards
                 Value = "",
             });
 
-            monthName = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1 % 12, 1).ToString("MMMM");
-            card.Body.Add(new AdaptiveTextBlock($"Forecast for {monthName}"));
+            var month2Name = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1 % 12, 1).ToString("MMMM");
+            card.Body.Add(new AdaptiveTextBlock($"Forecast for {month2Name}"));
             card.Body.Add(new AdaptiveCards.AdaptiveTextInput()
             {
                 Id = "forecast2",
                 Placeholder = "0",
                 Value = "",
             });
-            monthName = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 2 % 12, 1).ToString("MMMM");
-            card.Body.Add(new AdaptiveTextBlock($"Forecast for {monthName}"));
+            var month3Name = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 2 % 12, 1).ToString("MMMM");
+            card.Body.Add(new AdaptiveTextBlock($"Forecast for {month3Name}"));
             card.Body.Add(new AdaptiveCards.AdaptiveTextInput()
             {
                 Id = "forecast3",
@@ -92,9 +92,16 @@ namespace ConsultingBot.Cards
                 Value = "",
             });
 
-            var payload = new CardActionValue()
+            var payload = new ProjectAssignmentCardActionValue()
             {
-                submissionId = ProjectAssignmentCard.SubmissionId
+                submissionId = ProjectAssignmentCard.SubmissionId,
+                clientUrl = requestDetails.project.Client.LogoUrl,
+                clientName = requestDetails.project.Client.Name,
+                projectName = requestDetails.project.Name,
+                personName = requestDetails.personName,
+                forecastMonth1 = month1Name,
+                forecastMonth2 = month2Name,
+                forecastMonth3 = month3Name,
             };
 
             CardAction action = new CardAction()
@@ -112,15 +119,36 @@ namespace ConsultingBot.Cards
         public static async Task<InvokeResponse> HandleInvokeActivityAsync(ITurnContext turnContext)
         {
             var val = turnContext.Activity.Value as JObject;
-            var payload = val.ToObject<CardSubmittedValue>();
+            var payload = val.ToObject<ProjectAssignmentCardSubmitValue>();
 
+            var responseCard = ProjectAssignmentConfirmationCard.GetCard(payload);
+            Activity replyActivity = turnContext.Activity.CreateReply();
+            replyActivity.Attachments = new List<Attachment>()
+                {
+                    responseCard.ToAttachment()
+                };
+            await turnContext.SendActivityAsync(replyActivity).ConfigureAwait(false);
             return await Task.FromResult<InvokeResponse>(null);
         }
 
         public static string SubmissionId { get; } = nameof(ProjectAssignmentCard) + "submit";
 
-        public class CardSubmittedValue : CardActionValue
+
+        public class ProjectAssignmentCardActionValue : CardActionValue
         {
+            // From request details
+            public string clientUrl = "";
+            public string clientName = "";
+            public string projectName = "";
+            public string personName = "";
+            public string forecastMonth1 = "";
+            public string forecastMonth2 = "";
+            public string forecastMonth3 = "";
+        }
+
+        public class ProjectAssignmentCardSubmitValue : ProjectAssignmentCardActionValue
+        {
+            // From adaptive card
             public string roleChoice = "";
             public string forecast1 = "0";
             public string forecast2 = "0";
---
+++ ConsultingBot/ConsultingBot/Cards/ProjectAssignmentConfirmationCard.cs
@@ -0,0 +1,57 @@
+﻿using AdaptiveCards;
+using ConsultingBot.InvokeActivityHandlers;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsultingBot.Cards
+{
+    public static class ProjectAssignmentConfirmationCard
+    {
+        public static AdaptiveCard GetCard(ProjectAssignmentCard.ProjectAssignmentCardSubmitValue value)
+        {
+            var card = new AdaptiveCard();
+            var factSet = new AdaptiveFactSet();
+            factSet.Facts.Add(new AdaptiveFact("Client", value.clientName));
+            factSet.Facts.Add(new AdaptiveFact("Project", value.projectName));
+            factSet.Facts.Add(new AdaptiveFact("Consultant", value.personName));
+            factSet.Facts.Add(new AdaptiveFact("Role", value.roleChoice));
+            factSet.Facts.Add(new AdaptiveFact($"Forecast for {value.forecastMonth1}",
+                String.IsNullOrEmpty(value.forecast1) ? "0" : value.forecast1));
+            factSet.Facts.Add(new AdaptiveFact($"Forecast for {value.forecastMonth2}",
+                String.IsNullOrEmpty(value.forecast2) ? "0" : value.forecast2));
+            factSet.Facts.Add(new AdaptiveFact($"Forecast for {value.forecastMonth3}",
+                String.IsNullOrEmpty(value.forecast3) ? "0" : value.forecast3));
+
+            card.Body.Add(new AdaptiveColumnSet()
+            {
+                Columns = new List<AdaptiveCards.AdaptiveColumn>()
+                {
+                    new AdaptiveColumn()
+                    {
+                        Width = "35%",
+                        Items = new List<AdaptiveElement>()
+                        {
+                            new AdaptiveImage(value.clientUrl)
+                        }
+                    },
+                    new AdaptiveColumn()
+                    {
+                        Width = "65%",
+                        Items = new List<AdaptiveElement>()
+                        {
+                             factSet
+                        }
+                    },
+                }
+            });
+
+            return card;
+        }
+    }
+}
